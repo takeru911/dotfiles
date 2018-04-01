022602a7b2506c50b5e77d474a5d50baa0548f2c
takeru911 <takeru_maehara@voyagegroup.com>
2018-04-01 07:51:05 +0200

    flycheck pop

--- emacs/.emacs.d/inits/38-flycheck-conf.el
+++ emacs/.emacs.d/inits/38-flycheck-conf.el
@@ -1,8 +1,7 @@
 ;;flycheck
 (setq flycheck-display-errors-delay 0.3)
 (add-hook 'after-init-hook #'global-flycheck-mode)
-(eval-after-load 'flycheck
-  '(custom-set-variables
-   '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+(with-eval-after-load 'flycheck
+  (flycheck-pos-tip-mode))
 (global-set-key (kbd "\C-x C-n")  'flycheck-next-error)
 (global-set-key (kbd "\C-x C-p") 'flycheck-previous-error)
